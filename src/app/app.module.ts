c124160b76cc0ea42d1a17ccd09e8beec81dfcd5
401 <401@academicos.cesde.edu.co>
2022-05-10 11:22:49 -0500

    Bases de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NoticiasComponent } from './shared/noticias/noticias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    NoticiasComponent
   ],
   imports: [
     BrowserModule,
